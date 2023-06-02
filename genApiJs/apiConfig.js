420bfbfe80e0c1cb3779fce6d77f91bda55b7a43
cjh <1964646121@qq.com>
2023-06-02 15:22:56 +0800

    feat: 文件目录变更

--- genApiJs/apiConfig.js
+++ genApiJs/apiConfig.js
@@ -4,7 +4,7 @@ module.exports = {
   apiList: [
     {
       swaggerUrl: './testData/swagger1.json', // 相对根目录的路径
-      outputDir: '/api/swagger1.js', // 输出到哪个目录中
+      outputDir: '/outputJs/swagger1.js', // 输出到哪个目录中
       tag: true, // 是否生成
       prefix: '', // 前缀, 如 /abc, 则生成的接口将是  Http.post('/abc/xxx/xxx')
       ignore: /\/basePlatform\/|\/test\//, // 路径中带 /basePlatform/ 和 /test/ 的接口不用生成
