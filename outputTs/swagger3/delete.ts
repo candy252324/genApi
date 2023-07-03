58506cbee376925d96722c360761d0edf287677f
cjh <1964646121@qq.com>
2023-07-03 20:17:28 +0800

    根据swagger生成所有interface

--- outputTs/swagger3/delete.ts
+++ outputTs/swagger3/delete.ts
@@ -4,6 +4,6 @@ import { AxiosPromise, AxiosRequestConfig } from 'axios'
 import {ApiResponse} from './_interfaces'
 
 /** 管理员删除群成员 */
-export function delete1  (data:any, config?: AxiosRequestConfig) :AxiosPromise<ApiResponse>{
+export function delete  (data:any, config?: AxiosRequestConfig) :AxiosPromise<ApiResponse>{
   return Http.post('/delete', data, config)
 }
