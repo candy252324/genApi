d77d8315762b3ac0deb61c488a68b81ab70d6243
cjh <1964646121@qq.com>
2023-11-24 10:07:46 +0800

    目录变更

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,14 @@
 const typescript = require('rollup-plugin-typescript2')
 
 module.exports = {
-  input: 'core2/index.ts',
+  input: 'core/index.ts',
   output: {
     file: 'dist/index.js',
     format: 'cjs',
   },
   plugins: [
     typescript({
-      include: ['core2/**'],
+      include: ['core/**'],
     }),
   ],
 }
