a8b22bd2fda8b63e70072c3e904a7e121940ab39
cjh <1964646121@qq.com>
2024-07-16 16:33:59 +0800

    feat: 修改lint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,9 @@ module.exports = {
   },
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  extends: ['plugin:@typescript-eslint/recommended'],
+  extends: ['plugin:@typescript-eslint/recommended', 'eslint:recommended'],
   rules: {
+    'no-unused-vars': 'warn',
     '@typescript-eslint/no-unused-vars': 'warn', // 未使用的变量
     '@typescript-eslint/no-explicit-any': 'off', // 是否允许使用any
     '@typescript-eslint/ban-types': 'off', // https://typescript-eslint.io/rules/ban-types/
